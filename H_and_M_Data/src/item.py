38b2462bf6eb7e1cf0c748dd0b6427c9ac86e93b
Diarra Bell <dbell@6river.com>
2023-05-04 14:14:21 -0400

    debugging

--- H_and_M_Data/src/item.py
+++ H_and_M_Data/src/item.py
@@ -29,7 +29,7 @@ class ItemFeatures:
         item_properties_df['article_id'] = item_encoder.transform(item_properties_df.article_id)
         #create label encoder and encode item properties 
         le = LabelEncoder()
-        cols = item_properties_df.columns
+        cols = item_properties_df.columns.values.tolist()
         print(cols, type(cols))
         cols = cols.remove('article_id') 
         print(cols)
